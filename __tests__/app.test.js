beb5480047d539f5fa0bc8db06f54a9b00135dca
Yousuff <yousufftanveer77@gmail.com>
2022-12-13 13:30:25 +0000

    completes ticket 3 with happy path TDD

---
+++ __tests__/app.test.js
@@ -0,0 +1,39 @@
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
+
+beforeEach(() => seed(testData));
+
+afterAll(() => {
+  if (db.end) db.end();
+});
+
+describe("Get api/topics", () => {
+  test("Status 200: Should return with an array length of 3", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        console.log(body);
+        expect(body.length).toBe(3);
+      });
+  });
+  test("Should return with an array of object, all of which have a slug and description key  ", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const topicsArr = body;
+        topicsArr.forEach((topic) => {
+          expect(topic).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const { getTopics } = require("./controllers/controller");
+
+app.get("/api/topics", getTopics);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "internal server error" });
+});
+
+module.exports = app;
