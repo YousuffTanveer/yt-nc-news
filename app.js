64ceb5c2857e1d89bbe89f1c316e8052c7ddc7f8
Yousuff <yousufftanveer77@gmail.com>
2023-01-09 12:07:56 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const {
   getTopics,
   getArticles,
   getArticleById,
   getComments,
+  postComments,
+  patchArticles,
 } = require("./controllers/controller");
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles/:article_id/comments", getComments);
+
+app.post("/api/articles/:article_id/comments", postComments);
+
+app.patch("api/articles/:article_id", patchArticles);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "invalid endpoint path" });
@@ -26,6 +36,8 @@ app.use((err, req, res, next) => {
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "invalid id" });
+  } else if (err.code === "23503") {
+    res.status(400).send({ msg: "bad request" });
   } else {
     next(err);
   }
